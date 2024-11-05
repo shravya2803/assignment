b8edf10b891fe04bc6be3ce5d9fab9e3fa2daf56
shravya2803 <u24ai090@aid.svnit.ac.in>
2024-11-05 13:16:13 +0530

    Delete Shravya directory

--- Shravya/a12.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    char x;
-    printf("PLeasse enter the chartar\n");
-    scanf("%c",&x);
-    printf("the ascii code of the char is %d",x);
-}
--- Shravya/a4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    double fer;
-    printf("PLease enter tempture in fer\n");
-    scanf("%lf",&fer);
-    double deg=(((fer-32)*5)/9);
-    printf("tempture celsus are %lf\n",deg);
-}
